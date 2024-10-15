f5a722e5dfc846102e5bda29c9623d2fe94b7db3
JomanaMahmoud <jomanamahmoud53@gmail.com>
2024-10-15 16:33:45 +0300

    added when the keys are clicked it plays the sounds

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ let keys = document.getElementsByClassName("key");
 
     document.addEventListener('keydown', function(event) {
         let keyPressed = event.key.toUpperCase();
+        console.log("Key pressed: " + keyPressed);
+        
         for (let i = 0; i < keys.length; i++) {
-            if (keys[i].getAttribute("data-key") === keyPressed) {
-                let sound = new Audio(keys[i].getAttribute("data-sound"));
-                sound.play();
+            
+            if (keys[i].innerText === keyPressed) {
+                let soundFile = keys[i].getAttribute("data-sound");
+                console.log("Key matched: " + keyPressed + " Sound file: " + soundFile);
+                
+                if (soundFile) {
+                    let sound = new Audio(soundFile);
+                    sound.play();
+                } else {
+                    console.log("Sound file not found for this key");
+                }
             }
         }
     });
